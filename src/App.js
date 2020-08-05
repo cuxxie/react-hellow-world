62233789ece4afa1744896e182b245a26dbaa7ec
Bird.AC <hello@bird.ac>
2020-08-05 22:19:04 +0700

    aabbcc

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. But no..
+          Edit <code>src/App.js</code> and save to reload. But no.. please dont
         </p>
         <a
           className="App-link"
