14e7b31ecee17aed1a04dda92e80151c0a19cc28
Bird.AC <hello@bird.ac>
2020-08-06 00:17:04 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. New repo.. updated 07777
+          Edit <code>src/App.js</code> and save to reload. New repo.. updated 77777
         </p>
         <a
           className="App-link"
