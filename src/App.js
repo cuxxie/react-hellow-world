9213ffec2540f00e7ae1e41865aabd2c99b58ace
Bird.AC <hello@bird.ac>
2020-08-06 19:44:27 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. New repo.. Kelas CI/CD
+          Edit <code>src/App.js</code> and save to reload. New repo..
         </p>
         <a
           className="App-link"
