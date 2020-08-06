099349362db1c218af1e25cae617729d5257690d
Hendri Wang <hendri.wang88@gmail.com>
2020-08-06 21:03:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. New repo..
+          Edit <code>src/App.js</code> and save to reload. New repo.. Deploy to VPS!
         </p>
         <a
           className="App-link"
